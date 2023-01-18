3b35b1d62a88bdfd01a955f49dd522d8fd46a857
Sheishori <sheishori@gmail.com>
2023-01-18 14:18:06 +0100

    Add a form for a new education element

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,6 +2,26 @@ import React from 'react';
 
 class Education extends React.Component {
 	render() {
+		const newForm = (
+			<form>
+				<ul>
+					<li>
+						<label htmlFor='school-name'>School name:</label>
+						<input type='text' name='school-name' />
+					</li>
+					<li>
+						<label htmlFor='title'>Title of study:</label>
+						<input type='text' name='title' />
+					</li>
+					<li>
+						<label htmlFor='date'>Date of study:</label>
+						<input type='text' name='date' />
+					</li>
+				</ul>
+				<button>Save</button>
+			</form>
+		);
+
 		return (
 			<div id='Education'>
 				<p>Education</p>
