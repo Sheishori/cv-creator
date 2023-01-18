ca74982f4a0812535b379830baa6ed0688f918e4
Sheishori <sheishori@gmail.com>
2023-01-18 14:24:10 +0100

    Display a form when clicked on the add button in the education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class Education extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			editing: false,
+		};
+
+		this.newEducation = this.newEducation.bind(this);
+	}
+
+	newEducation() {
+		this.setState({
+			editing: true,
+		});
+	}
+
 	render() {
+		let editing = '';
 		const newForm = (
 			<form>
 				<ul>
@@ -22,10 +39,15 @@ class Education extends React.Component {
 			</form>
 		);
 
+		if (this.state.editing) {
+			editing = newForm;
+		}
+
 		return (
 			<div id='Education'>
 				<p>Education</p>
-				<button>Add</button>
+				{editing}
+				<button onClick={this.newEducation}>Add</button>
 			</div>
 		);
 	}
