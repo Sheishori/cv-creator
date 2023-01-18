64f4b7e4d1d6dde0e4e22564e93cd1f3f9c7ddec
Sheishori <sheishori@gmail.com>
2023-01-18 12:42:00 +0100

    Add a form in PersonalInfo component

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class PersonalInfo extends React.Component {
 	render() {
-		return <div id='PersonalInfo'></div>;
+		return (
+			<form id='PersonalInfo'>
+				<p>Personal Information</p>
+				<ul>
+					<li>
+						<label htmlFor='name'>Name:</label>
+						<input type='text' name='name' />
+					</li>
+					<li>
+						<label htmlFor='email'>E-mail:</label>
+						<input type='text' name='email' />
+					</li>
+					<li>
+						<label htmlFor='phone'>Phone:</label>
+						<input type='text' name='phone' />
+					</li>
+				</ul>
+			</form>
+		);
 	}
 }
 
