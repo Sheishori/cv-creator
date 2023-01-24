1fad122ed65ac329ef01b8f32f8f295da1588373
Sheishori <sheishori@gmail.com>
2023-01-24 14:48:28 +0100

    Rewrite Education as a function component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,215 +1,179 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import '../styles/Education.css';
 
-class Education extends React.Component {
-	constructor(props) {
-		super(props);
+function Education(props) {
+	const [education, setEducation] = useState([
+		{
+			school: 'Technological University',
+			title: "Master's Degree in IT",
+			startDate: '2012',
+			endDate: '2016',
+			id: 789,
+		},
+	]);
 
-		this.state = {
-			education: [
-				{
-					school: 'Technological University',
-					title: "Master's Degree in IT",
-					startDate: '2012',
-					endDate: '2016',
-					id: 789,
-				},
-			],
-			new: {
-				school: '',
-				title: '',
-				startDate: '',
-				endDate: '',
-				id: uniqid(),
-			},
-			editing: false,
-		};
+	const [newEdu, setNewEdu] = useState({
+		school: '',
+		title: '',
+		startDate: '',
+		endDate: '',
+		id: uniqid(),
+	});
 
-		this.newEducation = this.newEducation.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.saveEducation = this.saveEducation.bind(this);
-		this.editEducation = this.editEducation.bind(this);
-		this.delEducation = this.delEducation.bind(this);
-		this.cancel = this.cancel.bind(this);
+	const [editingEdu, setEditingEdu] = useState(false);
+
+	function newEducation() {
+		setEditingEdu(true);
 	}
 
-	newEducation() {
-		this.setState({
-			editing: true,
-		});
-	}
-
-	handleChange(event) {
+	function handleChange(event) {
 		const target = event.target;
 		const value = target.value;
 		const name = target.name;
 
-		this.setState({
-			new: {
-				...this.state.new,
-				[name]: value,
-			},
-		});
+		setNewEdu({ ...newEdu, [name]: value });
 	}
 
-	saveEducation(event) {
+	function saveEducation(event) {
 		event.preventDefault();
 		let edu = [];
 
 		{
 			/* Check whether to edit or add a new element to the experience array */
 		}
-		if (
-			this.state.education.find((element) => element.id === this.state.new.id)
-		) {
-			edu = this.state.education.reduce((all, current) => {
-				if (current.id === this.state.new.id) return all.concat(this.state.new);
+		if (education.find((element) => element.id === newEdu.id)) {
+			edu = education.reduce((all, current) => {
+				if (current.id === newEdu.id) return all.concat(newEdu);
 				else return all.concat(current);
 			}, []);
-		} else edu = this.state.education.concat(this.state.new);
+		} else edu = education.concat(newEdu);
 
-		this.setState({
-			education: edu,
-			new: {
-				school: '',
-				title: '',
-				startDate: '',
-				endDate: '',
-				id: uniqid(),
-			},
-			editing: false,
+		setEducation(edu);
+		setNewEdu({
+			school: '',
+			title: '',
+			startDate: '',
+			endDate: '',
+			id: uniqid(),
 		});
+		setEditingEdu(false);
 	}
 
-	cancel(event) {
+	function cancel(event) {
 		event.preventDefault();
-		this.setState({
-			new: {
-				school: '',
-				title: '',
-				startDate: '',
-				endDate: '',
-				id: uniqid(),
-			},
-			editing: false,
+		setNewEdu({
+			school: '',
+			title: '',
+			startDate: '',
+			endDate: '',
+			id: uniqid(),
 		});
+		setEditingEdu(false);
 	}
 
-	editEducation(id) {
-		let edit = this.state.education.find((element) => element.id === id);
-		this.setState({
-			new: edit,
-			editing: true,
-		});
+	function editEducation(id) {
+		let edit = education.find((element) => element.id === id);
+		setNewEdu(edit);
+		setEditingEdu(true);
 	}
 
-	delEducation(id) {
-		this.setState({
-			education: this.state.education.filter((element) => element.id !== id),
-		});
+	function delEducation(id) {
+		setEducation(education.filter((element) => element.id !== id));
 	}
 
-	render() {
-		const { state, props } = this;
+	let editing = '';
+	const newForm = (
+		<form>
+			<ul>
+				<li>
+					<label htmlFor='school'>School name:</label>
+					<input
+						type='text'
+						name='school'
+						value={newEdu.school}
+						onChange={handleChange}
+					/>
+				</li>
+				<li>
+					<label htmlFor='title'>Title of study:</label>
+					<input
+						type='text'
+						name='title'
+						value={newEdu.title}
+						onChange={handleChange}
+					/>
+				</li>
+				<li>
+					<label htmlFor='startDate'>Start date:</label>
+					<input
+						type='text'
+						name='startDate'
+						value={newEdu.startDate}
+						onChange={handleChange}
+					/>
+				</li>
+				<li>
+					<label htmlFor='endDate'>End date:</label>
+					<input
+						type='text'
+						name='endDate'
+						value={newEdu.endDate}
+						onChange={handleChange}
+					/>
+				</li>
+			</ul>
+			<button onClick={saveEducation}>Save</button>
+			<button onClick={cancel}>Cancel</button>
+		</form>
+	);
 
-		let editing = '';
-		const newForm = (
-			<form>
-				<ul>
-					<li>
-						<label htmlFor='school'>School name:</label>
-						<input
-							type='text'
-							name='school'
-							value={state.new.school}
-							onChange={this.handleChange}
-						/>
-					</li>
-					<li>
-						<label htmlFor='title'>Title of study:</label>
-						<input
-							type='text'
-							name='title'
-							value={state.new.title}
-							onChange={this.handleChange}
-						/>
-					</li>
-					<li>
-						<label htmlFor='startDate'>Start date:</label>
-						<input
-							type='text'
-							name='startDate'
-							value={state.new.startDate}
-							onChange={this.handleChange}
-						/>
-					</li>
-					<li>
-						<label htmlFor='endDate'>End date:</label>
-						<input
-							type='text'
-							name='endDate'
-							value={state.new.endDate}
-							onChange={this.handleChange}
-						/>
-					</li>
-				</ul>
-				<button onClick={this.saveEducation}>Save</button>
-				<button onClick={this.cancel}>Cancel</button>
-			</form>
-		);
+	const addButton = <button onClick={newEducation}>Add</button>;
+	let button = addButton;
 
-		const addButton = <button onClick={this.newEducation}>Add</button>;
-		let button = addButton;
+	if (editingEdu) {
+		editing = newForm;
+		button = '';
+	}
 
-		if (state.editing) {
-			editing = newForm;
-			button = '';
-		}
+	if (props.editing === false) {
+		editing = '';
+		button = '';
+	}
 
-		if (props.editing === false) {
-			editing = '';
-			button = '';
-		}
+	let active = '';
+	if (props.editing === false) {
+		active = 'hidden';
+	}
 
-		let active = '';
-		if (props.editing === false) {
-			active = 'hidden';
-		}
-
-		return (
-			<div id='Education'>
-				<h3>Education</h3>
-				<ul className='all'>
-					{state.education.map((element) => {
-						return (
-							<li key={element.id}>
-								<div className='education-info'>
-									<div className='date'>
-										{element.startDate} - {element.endDate}
-									</div>
-									<div className='school-info'>
-										<div className='school-name'>{element.school}</div>
-										<div className='title'>{element.title}</div>
-									</div>
+	return (
+		<div id='Education'>
+			<h3>Education</h3>
+			<ul className='all'>
+				{education.map((element) => {
+					return (
+						<li key={element.id}>
+							<div className='education-info'>
+								<div className='date'>
+									{element.startDate} - {element.endDate}
 								</div>
-								<div className={'buttons ' + active}>
-									<button onClick={() => this.editEducation(element.id)}>
-										Edit
-									</button>
-									<button onClick={() => this.delEducation(element.id)}>
-										Delete
-									</button>
+								<div className='school-info'>
+									<div className='school-name'>{element.school}</div>
+									<div className='title'>{element.title}</div>
 								</div>
-							</li>
-						);
-					})}
-				</ul>
-				{editing}
-				{button}
-			</div>
-		);
-	}
+							</div>
+							<div className={'buttons ' + active}>
+								<button onClick={() => editEducation(element.id)}>Edit</button>
+								<button onClick={() => delEducation(element.id)}>Delete</button>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+			{editing}
+			{button}
+		</div>
+	);
 }
 
 export default Education;
