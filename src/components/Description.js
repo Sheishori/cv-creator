1ad331ad3817f05f4277bf8048e2cf61003ba723
Sheishori <sheishori@gmail.com>
2023-01-24 14:14:16 +0100

    Rewrite Description as a function component

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,71 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Description.css';
 
-class Description extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada accumsan ligula, sed bibendum tortor elementum sit amet. Aliquam lacus nunc, finibus mattis feugiat a, tristique a tortor. Aliquam suscipit non tortor a molestie. In hac habitasse platea dictumst. Praesent quis aliquam enim. Nulla sit amet erat vitae orci posuere ultrices. Cras gravida diam arcu, posuere molestie lectus pellentesque a. Aliquam ultricies hendrerit ex. Nam urna diam, vestibulum sed eleifend vel, euismod id libero.',
-			editing: true,
-		};
-
-		this.handleChange = this.handleChange.bind(this);
-		this.saveInfo = this.saveInfo.bind(this);
-		this.editInfo = this.editInfo.bind(this);
+function Description(props) {
+	const [desc, setDesc] = useState(
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada accumsan ligula, sed bibendum tortor elementum sit amet. Aliquam lacus nunc, finibus mattis feugiat a, tristique a tortor. Aliquam suscipit non tortor a molestie. In hac habitasse platea dictumst. Praesent quis aliquam enim. Nulla sit amet erat vitae orci posuere ultrices. Cras gravida diam arcu, posuere molestie lectus pellentesque a. Aliquam ultricies hendrerit ex. Nam urna diam, vestibulum sed eleifend vel, euismod id libero.'
+	);
+	function handleChange(event) {
+		setDesc(event.target.value);
 	}
 
-	handleChange(event) {
-		this.setState({
-			desc: event.target.value,
-		});
-	}
-
-	saveInfo(event) {
+	const [editing, setEditing] = useState(true);
+	function saveInfo(event) {
 		event.preventDefault();
-		this.setState({
-			editing: false,
-		});
+		setEditing(false);
 	}
 
-	editInfo(event) {
+	function editInfo(event) {
 		event.preventDefault();
-		this.setState({
-			editing: true,
-		});
+		setEditing(true);
 	}
 
-	render() {
-		const { state, props } = this;
+	const descInput = (
+		<textarea name='desc' value={desc} onChange={handleChange} />
+	);
+	const descDiv = <div>{desc}</div>;
+	const submitButton = <button onClick={saveInfo}>Save</button>;
+	const editButton = <button onClick={editInfo}>Edit</button>;
 
-		const descInput = (
-			<textarea name='desc' value={state.desc} onChange={this.handleChange} />
-		);
+	let descField = descInput;
+	let button = submitButton;
 
-		const descDiv = <div>{state.desc}</div>;
-		const submitButton = <button onClick={this.saveInfo}>Save</button>;
-		const editButton = <button onClick={this.editInfo}>Edit</button>;
-
-		let descField = descInput;
-		let button = submitButton;
-
-		if (state.editing === false || props.editing === false) {
-			descField = descDiv;
-			button = editButton;
-		}
-
-		if (props.editing === false) {
-			button = '';
-		}
-
-		return (
-			<div id='Description'>
-				<label htmlFor='desc'>Description</label>
-				{descField}
-				{button}
-			</div>
-		);
+	if (editing === false || props.editing === false) {
+		descField = descDiv;
+		button = editButton;
 	}
+
+	if (props.editing === false) {
+		button = '';
+	}
+
+	return (
+		<div id='Description'>
+			<label htmlFor='desc'>Description</label>
+			{descField}
+			{button}
+		</div>
+	);
 }
 
 export default Description;
